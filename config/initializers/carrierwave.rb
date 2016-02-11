e39b59d4c9f8c450692aa1f403cbe0223ad2be2f
Kevin Coleman <kevinjamescoleman.7@gmail.com>
2016-02-10 17:17:03 -0800

    Fixed keys and added images

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAIEOWO2KLHP6IXB5A',                        # required
-    aws_secret_access_key: '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'                      # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],                        # required
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]                      # required
   }
   config.fog_directory  = 'image-upload-sample'                          # required
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
