df448be4f9cf467a94e4e92b49f072228c9c180b
Kevin Coleman <kevinjamescoleman.7@gmail.com>
2016-02-11 17:25:31 -0800

    Added logic for if a memory only has one image, also made it so all the memories are rendered via ajax.

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,14 +1,18 @@
 class MemoriesController < ApplicationController
+	respond_to :html, :js
+
   def show
   	@memory = Memory.find(params[:id])
     @geojson = Array.new
     @geojson << @memory.geojsonify
     respond_to do |format|
-      format.html
       format.json { render json: @geojson }  # respond with the created JSON object
+      format.js
+      format.html { render nothing: true  }
     end
   end
 
   def index
+  	@memory = Memory.prioritized.first
   end
 end
